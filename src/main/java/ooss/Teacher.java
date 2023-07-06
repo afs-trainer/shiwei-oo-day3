888178979b5b4b216c89599920b2c163ffd21fb2
shiwei.fu <shiwei.fu@thoughtworks.com>
2023-07-06 16:16:27 +0800

    feat: make teach can join class

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,11 +1,24 @@
 package ooss;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends Person {
+    private List<Klass> klasses = new ArrayList<>();
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
     public String introduce() {
         return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return klasses.contains(klass);
+    }
+
+    public void assignTo(Klass klass) {
+        klasses.add(klass);
     }
 }
--- src/test/java/ooss/step4/TeacherTest.java
+++ src/test/java/ooss/step4/TeacherTest.java
@@ -9,27 +9,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TeacherTest {
 
-//    @Test
-//    public void should_return_false_when_belongsTo_given_teacher_does_not_teach_the_class() {
-//        Teacher jerry = new Teacher(1, "Jerry", 21);
-//        Klass klass = new Klass(1);
-//
-//        boolean belongsTo = jerry.belongsTo(klass);
-//
-//        assertFalse(belongsTo);
-//    }
-//
-//    @Test
-//    public void should_return_true_when_belongsTo_given_teacher_teaches_in_the_class() {
-//        Teacher jerry = new Teacher(1, "Jerry", 21);
-//        Klass klass = new Klass(1);
-//        jerry.assignTo(klass);
-//
-//        boolean belongsTo = jerry.belongsTo(klass);
-//
-//        assertTrue(belongsTo);
-//    }
-//
+    @Test
+    public void should_return_false_when_belongsTo_given_teacher_does_not_teach_the_class() {
+        Teacher jerry = new Teacher(1, "Jerry", 21);
+        Klass klass = new Klass(1);
+
+        boolean belongsTo = jerry.belongsTo(klass);
+
+        assertFalse(belongsTo);
+    }
+
+    @Test
+    public void should_return_true_when_belongsTo_given_teacher_teaches_in_the_class() {
+        Teacher jerry = new Teacher(1, "Jerry", 21);
+        Klass klass = new Klass(1);
+        jerry.assignTo(klass);
+
+        boolean belongsTo = jerry.belongsTo(klass);
+
+        assertTrue(belongsTo);
+    }
+
 //    @Test
 //    public void should_all_return_true_when_belongsTo_given_teacher_teaches_multiple_classes() {
 //        Teacher jerry = new Teacher(1, "Jerry", 21);
