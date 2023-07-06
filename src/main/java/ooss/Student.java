7e8d79e9975976a6bdd5a43df97af79bb906586c
shiwei.fu <shiwei.fu@thoughtworks.com>
2023-07-06 15:43:32 +0800

    refactor: make student extend person

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,23 +1,12 @@
 package ooss;
 
-public class Student {
-    private int id;
-    private String name;
-    private int age;
+public class Student extends Person {
 
     public Student(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name, age);
     }
 
     public String introduce() {
         return String.format("My name is %s. I am %d years old. I am a student.", name, age);
-    }
-
-    @Override
-    public boolean equals(Object otherStudent) {
-        Student other = (Student) otherStudent;
-        return this.id == other.id;
     }
 }
