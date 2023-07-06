1f1e93817edde6a3e8748c8d6b9ba2cb4ebe4036
shiwei.fu <shiwei.fu@thoughtworks.com>
2023-07-06 15:17:05 +0800

    feat: make person retrun msg when introduce

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,4 +1,11 @@
 package ooss;
 
 public class Person {
+    public Person(int id, String name, int age) {
+
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 18 years old.";
+    }
 }
